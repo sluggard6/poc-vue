1f3f4ef804c0f6c96ce342c93f25c8cb02bfeff2
wufang <sydra@163.com>
2022-09-18 22:30:51 +0800

    first

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <span>登录系统</span>
+    <el-form>
+      <el-input type="text" v-model="username" placeholder="用户名"/>
+      <el-input type="password" v-model="password" placeholder="密码"/>
+    </el-form>
+  </main>
+</template>
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                username: "",
+                password: ""
+            }
+        }
+    }
+</script>
